e5b33c77566253a59f1660a576a8543f2e2e4359
Daniel Angell <me@danangell.com>
2025-07-23 18:46:21 -0700

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,12 @@ export default function RootLayout({
       <head>
         <title>Double the pendulums, double the fun</title>
         <meta name="description" content="A double pendulum simulator and parameter space explorer" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
